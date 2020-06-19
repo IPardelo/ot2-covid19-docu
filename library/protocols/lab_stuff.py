9a875fbf81584c60ed60aee0916de7a92b325b1e
Ismael Castiñeira <info@osmeusproxectos.es>
2020-06-19 13:23:18 +0200

    The area calculation changes as you choose tube

--- library/protocols/lab_stuff.py
+++ library/protocols/lab_stuff.py
@@ -1,3 +1,6 @@
+import math
+
+
 # following volumes in ul
 def brands(brand_name):
     brands = {
@@ -27,56 +30,71 @@ def tubes(tube_tipe):
         'falcon': {
             'volume': 4000,
             'diameter': 28,
-            'hcone': 17.4
+            'hcono': 14 #17.4
         },
         'ependor1.5': {
             'volume': 1500,
             'diameter': 9,
-            'hcone': 17
+            'hcono': 19
         },
         'ependor2': {
             'volume': 2000,
             'diameter': 9,
-            'hcone': 0
+            'hcono': 19
         },
-        'congelacion': {
+        'criotubo': {
             'volume': 2000,
-            'diameter': 0,
-            'hcone': 0
+            'diameter': 8,
+            'hcono': 2
         },
         'f_redondo': {
             'volume': 3000,
             'diameter': 9,
-            'hcone': 3
+            'hcono': 3
         }
     }
     volume = tubes.get(tube_tipe).get('volume')
     diameter = tubes.get(tube_tipe).get('diameter')
-    hcone = tubes.get(tube_tipe).get('hcone')
-    return volume, diameter, hcone
+    hcono = tubes.get(tube_tipe).get('hcono')
+    #Calculos
+    area = (math.pi * diameter**2) / 4
+
+    if tube_tipe == 'falcon':
+        vcono = 1 / 3 * hcono * area
+        hcono = vcono * 3 / area
+    else:
+        vcono = 4 * area * diameter * 0.5 / 3
+    return area, volume, diameter, vcono, hcono
 
 def buffer(buffer_name):
     buffer = {
         'Lisis': {
             'flow_rate_aspirate': 1,  # multiplier
             'flow_rate_dispense': 1,  # multiplier
+            'delay': 0,  # delay after aspirate: to allow drops to fall before moving the pipette
+            'vol_well': 20000
         },
         'Roche Cobas': {
             'flow_rate_aspirate': 1,  # multiplier
             'flow_rate_dispense': 1,  # multiplier
+            'delay': 0,  # delay after aspirate: to allow drops to fall before moving the pipette
+            'vol_well': 20000
         },
         'UXL Longwood': {
             'flow_rate_aspirate': 1,  # multiplier
             'flow_rate_dispense': 1,  # multiplier
             'delay': 1,  # delay after aspirate: to allow drops to fall before moving the pipette
+            'vol_well': 20000
         },
         'Roche Bleau': {
             'flow_rate_aspirate': 1,  # multiplier
             'flow_rate_dispense': 1,  # multiplier
             'delay': 1,  # delay after aspirate: to allow drops to fall before moving the pipette
+            'vol_well': 20000
         },
     }
     flow_rate_aspirate = buffer.get(buffer_name).get('flow_rate_aspirate')
     flow_rate_dispense = buffer.get(buffer_name).get('flow_rate_dispense')
     delay = buffer.get(buffer_name).get('delay')
-    return flow_rate_aspirate, flow_rate_dispense, delay
+    vol_well = buffer.get(buffer_name).get('vol_well')
+    return flow_rate_aspirate, flow_rate_dispense, delay, vol_well
